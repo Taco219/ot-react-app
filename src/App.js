e279f4ddc8d0c2ef8c1ffae738f87b0733df6be1
thijsvandeven.vandeven@student.fontys.nl <thijsvandeven.vandeven@student.fontys.nl>
2023-03-29 13:19:15 +0200

    Test jenkins auto build 1

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <h1>Simple OT app to test jenkins</h1>
+                <p>Test change 1</p>
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
